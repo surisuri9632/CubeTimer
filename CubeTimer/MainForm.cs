bf564e1cc8380d0c4c9b093e7a0e709fb710e6a3
김현수 <surisuri9632@gmail.com>
2019-05-26 12:20:09 +0900

    초기 커밋.

---
+++ CubeTimer/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CubeTimer
+{
+    /// <summary>
+    /// 메인폼 클래스 입니다.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        //Constructor (Public)
+
+        #region MainForm() - 생성자 입니다.
+        
+        /// <summary>
+        /// 생성자 입니다.
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
